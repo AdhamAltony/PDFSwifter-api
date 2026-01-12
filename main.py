78f8b471a0fb9d6f202b2a17b45047a24acbb76e
Adham <adham.a.altony@gmail.com>
2026-01-12 13:28:56 +0200

    Remove prefixes from api

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ from app.routes.youtube import router as youtube_router
 from app.routes.downloads import router as downloads_router
 from app.routes.pdf import router as pdf_router
 
-API_PREFIX = "/tools"
-
 app = FastAPI(
-    openapi_url=f"{API_PREFIX}/openapi.json",
-    docs_url=f"{API_PREFIX}/docs",
-    redoc_url=f"{API_PREFIX}/redoc",
+    openapi_url="/openapi.json",
+    docs_url="/docs",
+    redoc_url="/redoc",
 )
 
-app.include_router(youtube_router, prefix=API_PREFIX)
-app.include_router(tiktok_router, prefix=API_PREFIX)
-app.include_router(downloads_router, prefix=API_PREFIX)
-app.include_router(pdf_router, prefix=API_PREFIX)
+app.include_router(youtube_router)
+app.include_router(tiktok_router)
+app.include_router(downloads_router)
+app.include_router(pdf_router)
